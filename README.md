ec95647d9d6bc3d63c2b05ea35e329386805f7e1
Andy Till <atill@mail.com>
2014-09-07 16:25:42 +0100

    make it so

---
+++ README.md
@@ -0,0 +1,8 @@
+
+# erlyberly
+
+erlyberly is a clone of the console program entop, rewritten in java/javafx.
+
+# Current State
+
+It's a dirty hack with the smallest amount of code required to get going.
---
+++ src/ProcInfo.java
@@ -0,0 +1,76 @@
+import java.util.Map;
+import java.util.Objects;
+
+import javafx.beans.property.LongProperty;
+import javafx.beans.property.SimpleLongProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+import com.ericsson.otp.erlang.OtpErlangAtom;
+import com.ericsson.otp.erlang.OtpErlangLong;
+import com.ericsson.otp.erlang.OtpErlangPid;
+
+public class ProcInfo {
+
+	private static final OtpErlangAtom REGISTERED_NAME_ATOM = new OtpErlangAtom(
+			"registered_name");
+
+	private static final OtpErlangAtom REDUCTIONS_ATOM = new OtpErlangAtom(
+			"reductions");
+
+	private StringProperty processName;
+
+	public void setProcessName(String value) {
+		processNameProperty().set(value);
+	}
+
+	public String getProcessName() {
+		return processNameProperty().get();
+	}
+
+	public StringProperty processNameProperty() {
+		if (processName == null)
+			processName = new SimpleStringProperty(this, "processName");
+		return processName;
+	}
+
+	private LongProperty reductions;
+
+	public void setReductions(long value) {
+		reductionsProperty().set(value);
+	}
+
+	public long getReductions() {
+		return reductionsProperty().get();
+	}
+
+	public LongProperty reductionsProperty() {
+		if (reductions == null)
+			reductions = new SimpleLongProperty(this, "reductions");
+		return reductions;
+	}
+
+	public static ProcInfo toProcessInfo(Map<Object, Object> propList) {
+		Object processName = propList.get(REGISTERED_NAME_ATOM);
+
+		if (processName == null) {
+			OtpErlangPid pid = (OtpErlangPid) propList.get("pid");
+			
+			// the target node is always zero to itself, I think!
+			processName = "<0." + pid.id() + "." + pid.serial() + ">";
+		}
+
+		ProcInfo processInfo;
+		processInfo = new ProcInfo();
+		processInfo.setProcessName(Objects.toString(processName, ""));
+		processInfo.setReductions(toInt(propList.get(REDUCTIONS_ATOM)));
+		return processInfo;
+	}
+
+	private static long toInt(Object object) {
+		if (object instanceof OtpErlangLong) {
+			return ((OtpErlangLong) object).longValue();
+		}
+		return 0;
+	}
+}
