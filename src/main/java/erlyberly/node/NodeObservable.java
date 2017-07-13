93e60d0ea4f37b09b0d43f813dd33ca1611e3a53
andytill <atill@basho.com>
2017-07-13 13:35:02 +0000

    Remove warning on NodeObservable

--- src/main/java/erlyberly/node/NodeObservable.java
+++ src/main/java/erlyberly/node/NodeObservable.java
@@ -1,6 +1,7 @@
 package erlyberly.node;
 
 import com.ericsson.otp.erlang.OtpErlangList;
+
 import erlyberly.ErlyBerly;
 import rx.Observable;
 import rx.schedulers.Schedulers;
@@ -9,13 +10,12 @@ import rx.schedulers.Schedulers;
  * Created by andytill on 23/11/2016.
  */
 public class NodeObservable  {
-    public static rx.Observable rpc(String mod, String function, OtpErlangList args) {
+    public static rx.Observable<?> rpc(String mod, String function, OtpErlangList args) {
         assert mod != null;
         assert function != null;
         assert args != null;
         return Observable.defer(() ->
                 Observable.just(ErlyBerly.nodeAPI().rpc(mod, function, args))
-        )
-                .observeOn(Schedulers.io());
+        ).observeOn(Schedulers.io());
     }
 }
